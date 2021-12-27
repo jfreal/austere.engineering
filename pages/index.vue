4af6804f18babc7d78728567758ecf28f988826b
John Farrell <johnefarrell@gmail.com>
2021-12-27 15:56:21 -0500

    adding test website

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+<div><img src="~/assets/logo.png" class="w-6"/></div>
+
+
+
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
